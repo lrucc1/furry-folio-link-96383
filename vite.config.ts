163f0fb1b56efa51f35fc9dd9243baaf325223e6
lrucc1 <40878238+lrucc1@users.noreply.github.com>
2025-12-01 21:23:15 +1100

    Improve environment detection for native builds

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    sourcemap: mode === "development",
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
